7df506c52d846080c9bc38e675dcea89cda6fb29
thanhbinhtest <hoangbinh15021998@gmail.com>
2025-01-16 16:12:04 +0700

    first commit

---
+++ Client/Client/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txb_CountryCode = new TextBox();
+            txb_CityName = new TextBox();
+            txb_District = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            dgvResults = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvResults).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(101, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Country code ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 95);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 20);
+            label2.TabIndex = 1;
+            label2.Text = "City Name ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(348, 28);
+            label3.Name = "label3";
+            label3.Size = new Size(96, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Distric name ";
+            // 
+            // txb_CountryCode
+            // 
+            txb_CountryCode.Location = new Point(158, 21);
+            txb_CountryCode.Name = "txb_CountryCode";
+            txb_CountryCode.Size = new Size(100, 27);
+            txb_CountryCode.TabIndex = 5;
+            // 
+            // txb_CityName
+            // 
+            txb_CityName.Location = new Point(158, 88);
+            txb_CityName.Name = "txb_CityName";
+            txb_CityName.Size = new Size(100, 27);
+            txb_CityName.TabIndex = 6;
+            // 
+            // txb_District
+            // 
+            txb_District.Location = new Point(482, 21);
+            txb_District.Name = "txb_District";
+            txb_District.Size = new Size(100, 27);
+            txb_District.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(616, 21);
+            button1.Name = "button1";
+            button1.Size = new Size(172, 47);
+            button1.TabIndex = 8;
+            button1.Text = "getAllCountry";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += getAllCountry_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(616, 82);
+            button2.Name = "button2";
+            button2.Size = new Size(172, 47);
+            button2.TabIndex = 9;
+            button2.Text = "getCountrybyCode";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += getCountrybyCode_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(616, 135);
+            button3.Name = "button3";
+            button3.Size = new Size(172, 47);
+            button3.TabIndex = 10;
+            button3.Text = "getCityByName";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += getCityByName_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(616, 251);
+            button4.Name = "button4";
+            button4.Size = new Size(172, 72);
+            button4.TabIndex = 11;
+            button4.Text = "getAllCityFromCountryCode";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += getAllCityFromCountryCode_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(616, 198);
+            button5.Name = "button5";
+            button5.Size = new Size(172, 47);
+            button5.TabIndex = 12;
+            button5.Text = "getCityByDistric";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += getCityByDistrict_Click;
+            // 
+            // dgvResults
+            // 
+            dgvResults.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvResults.Location = new Point(12, 179);
+            dgvResults.Name = "dgvResults";
+            dgvResults.Size = new Size(570, 259);
+            dgvResults.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dgvResults);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(txb_District);
+            Controls.Add(txb_CityName);
+            Controls.Add(txb_CountryCode);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvResults).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txb_CountryCode;
+        private TextBox txb_CityName;
+        private TextBox txb_District;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private DataGridView dgvResults;
+    }
+}
---
+++ Client/Client/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private async void getCountrybyCode_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var client = new ServiceReference1.WebService1SoapClient(ServiceReference1.WebService1SoapClient.EndpointConfiguration.WebService1Soap);
+
+                string countryCode = txb_CountryCode.Text.Trim();
+
+                if (string.IsNullOrEmpty(countryCode))
+                {
+                    MessageBox.Show("Please enter a valid country code.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                var response = await client.GetCountryByCodeAsync(countryCode);
+                var country = response.Body.GetCountryByCodeResult;
+
+                if (country != null)
+                {
+                    dgvResults.DataSource = null;
+                    dgvResults.DataSource = ConvertToDataTable(new[] { country },
+                        new[] { "Code", "Name", "Continent", "Region", "Population" },
+                        c => new object[] { c.Code, c.Name, c.Continent, c.Region, c.Population });
+                }
+                else
+                {
+                    MessageBox.Show("Country not found.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void getCityByName_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var client = new ServiceReference1.WebService1SoapClient(ServiceReference1.WebService1SoapClient.EndpointConfiguration.WebService1Soap);
+
+                string cityName = txb_CityName.Text.Trim();
+
+                if (string.IsNullOrEmpty(cityName))
+                {
+                    MessageBox.Show("Please enter a valid city name.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                var response = await client.GetCityByNameAsync(cityName);
+                var city = response.Body.GetCityByNameResult;
+
+                if (city != null)
+                {
+                    dgvResults.DataSource = null;
+                    dgvResults.DataSource = ConvertToDataTable(new[] { city },
+                        new[] { "ID", "Name", "CountryCode", "District", "Population" },
+                        c => new object[] { c.ID, c.Name, c.CountryCode, c.District, c.Population });
+                }
+                else
+                {
+                    MessageBox.Show("City not found.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void getAllCityFromCountryCode_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var client = new ServiceReference1.WebService1SoapClient(ServiceReference1.WebService1SoapClient.EndpointConfiguration.WebService1Soap);
+
+                string countryCode = txb_CountryCode.Text.Trim();
+
+                if (string.IsNullOrEmpty(countryCode))
+                {
+                    MessageBox.Show("Please enter a valid country code.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                var response = await client.GetAllCitiesByCountryCodeAsync(countryCode);
+                var cities = response.Body.GetAllCitiesByCountryCodeResult;
+
+                if (cities != null && cities.Length > 0)
+                {
+                    dgvResults.DataSource = null;
+                    dgvResults.DataSource = ConvertToDataTable(cities,
+                        new[] { "ID", "Name", "CountryCode", "District", "Population" },
+                        c => new object[] { c.ID, c.Name, c.CountryCode, c.District, c.Population });
+                }
+                else
+                {
+                    MessageBox.Show("No cities found for the given country.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void getCityByDistrict_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var client = new ServiceReference1.WebService1SoapClient(ServiceReference1.WebService1SoapClient.EndpointConfiguration.WebService1Soap);
+
+                string district = txb_District.Text.Trim();
+
+                if (string.IsNullOrEmpty(district))
+                {
+                    MessageBox.Show("Please enter a valid district name.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                var response = await client.GetCitiesByDistrictAsync(district);
+                var cities = response.Body.GetCitiesByDistrictResult;
+
+                if (cities != null && cities.Length > 0)
+                {
+                    dgvResults.DataSource = null;
+                    dgvResults.DataSource = ConvertToDataTable(cities,
+                        new[] { "ID", "Name", "CountryCode", "District", "Population" },
+                        c => new object[] { c.ID, c.Name, c.CountryCode, c.District, c.Population });
+                }
+                else
+                {
+                    MessageBox.Show("No cities found for the given district.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void getAllCountry_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var client = new ServiceReference1.WebService1SoapClient(ServiceReference1.WebService1SoapClient.EndpointConfiguration.WebService1Soap);
+
+                var response = await client.GetAllCountriesAsync();
+                var countries = response.Body.GetAllCountriesResult;
+
+                if (countries != null && countries.Length > 0)
+                {
+                    dgvResults.DataSource = null;
+                    dgvResults.DataSource = ConvertToDataTable(countries,
+                        new[] { "Code", "Name", "Continent", "Region", "Population" },
+                        c => new object[] { c.Code, c.Name, c.Continent, c.Region, c.Population });
+                }
+                else
+                {
+                    MessageBox.Show("No countries found.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private DataTable ConvertToDataTable<T>(T[] items, string[] columnNames, Func<T, object[]> rowValuesExtractor)
+        {
+            var table = new DataTable();
+
+            foreach (var col in columnNames)
+            {
+                table.Columns.Add(col);
+            }
+
+            foreach (var item in items)
+            {
+                table.Rows.Add(rowValuesExtractor(item));
+            }
+
+            return table;
+        }
+    }
+}
